823e427c9964d7d94f685eca9afb2cdc8eec8497
Lucas <luquipedro_orlan@hotmail.es>
2025-03-12 00:25:58 -0300

    timezone settled

--- utils.py
+++ utils.py
@@ -6,13 +6,14 @@ from constants import *
 def ill_remember_text_builder(cantidad, tipo):
     return u'I will remember it in ' + str(cantidad) + u' ' + tipo
 
+TIMEZONE = timezone(timedelta(hours=ADJUST_UTC))
 
 def calculate_start_date(month):
     # Adjusts years if month is greater than 12
-    actual_year = datetime.now(timezone.utc).year
+    actual_year = datetime.now(TIMEZONE).year
     adjusted_year = actual_year + (month - 1) // 12
     adjusted_month = (month - 1) % 12 + 1
-    start_date = datetime(adjusted_year, adjusted_month, 1).replace(tzinfo=timezone.utc)
+    start_date = datetime(adjusted_year, adjusted_month, 1).replace(tzinfo=TIMEZONE)
     return int(start_date.timestamp())
 
 def seg_to_str(seg):
